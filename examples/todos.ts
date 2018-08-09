fadd5ef73eea44acdc4e0e088cdfd291bf7e6d3b
Keloo <vlad-negura@mail.ru>
2018-08-09 13:45:35 +0300

    improving todo example

--- examples/todos.ts
+++ examples/todos.ts
@@ -15,3 +15,14 @@ async function todos() {
 }
 
 todos();
+
+/*
+[ { userId: 1,
+    id: 1,
+    title: 'delectus aut autem',
+    completed: false },
+  { userId: 1,
+    id: 2,
+    title: 'quis ut nam facilis et officia qui',
+    completed: false }]
+*/
