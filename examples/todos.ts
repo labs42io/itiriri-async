fbcd1bdeaef6e754be717eabc10fb7bb4344c908
Keloo <vlad-negura@mail.ru>
2018-08-09 13:56:41 +0300

    improving todo example

--- examples/todos.ts
+++ examples/todos.ts
@@ -8,12 +8,12 @@ async function* generator() {
   }
 }
 
-async function todos() {
+async function first2Uncompleted() {
   const q = await queryAsync(generator()).filter(x => x.completed === false).take(2).awaitAll();
   console.log(q.toArray());
 }
 
-todos();
+first2Uncompleted();
 
 /*
 [ { userId: 1,
@@ -24,4 +24,29 @@ todos();
     id: 2,
     title: 'quis ut nam facilis et officia qui',
     completed: false }]
+*/
+
+async function forAllCompleted() {
+  queryAsync(generator()).filter(e => e.completed === true).forEach(e => console.log(e));
+}
+
+forAllCompleted();
+
+/*
+{ userId: 1, id: 4, title: 'et porro tempora', completed: true }
+{ userId: 1,
+  id: 8,
+  title: 'quo adipisci enim quam ut ab',
+  completed: true }
+{ userId: 1,
+  id: 10,
+  title: 'illo est ratione doloremque quia maiores aut',
+  completed: true }
+{ userId: 1,
+  id: 11,
+  title: 'vero rerum temporibus dolor',
+  completed: true }
+...
+...
+...
 */
