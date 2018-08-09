f93e9a353613d1886cd30b90f5b18cf4f9a416f9
Keloo <vlad-negura@mail.ru>
2018-08-09 13:44:44 +0300

    todos examples

---
+++ examples/todos.ts
@@ -0,0 +1,17 @@
+import * as WebRequest from 'web-request';
+import { queryAsync } from '../lib';
+
+async function* generator() {
+  let id = 1;
+  while (true) {
+    var url = 'https://jsonplaceholder.typicode.com/todos/' + id++;
+    yield WebRequest.json<any>(url);
+  }
+}
+
+async function todos() {
+  const q = await queryAsync(generator()).filter(x => x.completed === false).take(2).awaitAll();
+  console.log(q.toArray());
+}
+
+todos();
